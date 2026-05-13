606016923f8de0898ce00e5b30aa55051c35baf9
thaleson <thaleson19@hotmail.com>
2026-05-13 20:02:55 -0300

    feat: add browser automation controller

---
+++ app/infrastructure/automation/browser_controller.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+
+class BrowserController:
+
+    @staticmethod
+    def open_youtube():
+
+        webbrowser.open("https://www.youtube.com")
--- app/services/command_service.py
+++ app/services/command_service.py
@@ -1,3 +1,6 @@
+from app.infrastructure.automation.browser_controller import BrowserController
+
+
 class CommandService:
 
     @staticmethod
@@ -6,6 +9,9 @@ class CommandService:
         normalized_command = command.lower()
 
         if "youtube" in normalized_command:
+
+            BrowserController.open_youtube()
+
             return "open_youtube"
 
         if "hora" in normalized_command:
